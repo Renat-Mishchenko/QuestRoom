bc058b516edf5687633600beae2374d3e55b96fb
3SHAPE\Renat Mishchenko <renat.mishchenko@3shape.com>
2024-02-28 11:58:00 +0200

    First commit

---
+++ QuestRoom/MainRoom.cs
@@ -0,0 +1,57 @@
+﻿namespace QuestRoom
+{
+    public class MainRoom
+    {
+        public int i = 0;
+        public string door;
+
+        public void EnterMainRoom() {
+            if (i == 0)
+                {
+                Console.WriteLine("You are brave enough to enter Quest Room... So, you' ve entered the Main room. You see three doors. " +
+                        "Two of them, on the right and on the left, are opened and waiting for visitors. The door in front of you has lock" +
+                        "with the password. Wich one you'll try?");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("[Left]");
+                Console.WriteLine("[Right]");
+                Console.WriteLine("[Front]");
+                door = Console.ReadLine();
+                i++;
+                DoorChoosing(door);
+            }
+            else
+                {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("You are it the Main Room. Wich door will you choose?");
+                Console.WriteLine("[Left]");
+                Console.WriteLine("[Right]");
+                Console.WriteLine("[Front]");
+                door = Console.ReadLine();
+                DoorChoosing(door);
+                }
+        }
+
+        public void DoorChoosing(string door)
+        {
+            if (door == "Left")
+            {
+
+            }
+            else if (door == "Rigth")
+            {
+
+            }
+            else if (door == "Front")
+            {
+                Console.WriteLine("You are near the front door. You see the lock, where code need to be inputed. Type the code");
+                string exitCode = Console.ReadLine();
+                if (exitCode == "1234")
+                {
+                    Console.WriteLine("You've exit Quest Room");
+                }
+
+            }
+        }
+            
+    }
+}
---
+++ QuestRoom/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace QuestRoom
+{
+    public class Start 
+    {
+        static void Main()
+        {
+            string startUserMessage = "";
+            FirstStep(startUserMessage);
+            void FirstStep(string startUserMessage)
+            {
+                Console.WriteLine("Hello! Type [Start] to enter the Quest Room, type [Exit] if you are scarried chicken");
+
+                startUserMessage = Console.ReadLine();
+                CheckStart(startUserMessage);
+            }
+
+
+            void CheckStart(string startUserMessage)
+            {
+                if (startUserMessage == "Start")
+                {
+                    MainRoom mainRoom = new MainRoom();
+                    mainRoom.EnterMainRoom();
+                }
+                else
+                {
+                    Console.WriteLine("You are the chicken! But you need to start the game");
+                    startUserMessage = Console.ReadLine();
+                    CheckStart(startUserMessage);
+                }
+            }
+        }
+    }    
+}
